b5759cde704cad4f5cf8b6e978f02f249f37f266
Gabriel Ricard <sumorai@gmail.com>
2016-10-19 06:58:58 -0400

    Added homepage & styles from react project

---
+++ src/components/HomePage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="jumbotron">
+        <h1>Pluralsight Administration</h1>
+        <p>React, Redux and React Router in ES6 for ultra-responsive web apps.</p>
+        <router-link to="/about" class="btn btn-primary btn-lg">Learn more</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HomePage'
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import Hello from './components/Hello.vue'
 import Goodbye from './components/Goodbye.vue'
 //import CoursesPage from './components/CoursesPage.vue'
 import Header from './components/Header';
+import HomePage from './components/HomePage';
 
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
@@ -20,13 +21,14 @@ const routes = [
     //{ path: '/courses', component: CoursesPage }
     { path: '/hello', component: Hello, name: 'hi' },
     { path: '/bye', component: Goodbye, name: 'bye' },
+    { path: '/', component: HomePage, name: 'home' },
     { path: '*', redirect: '/hello' }
 ];
 
 const router = new VueRouter({
     mode: 'history',
-    routes: routes
-//  linkActiveClass: 'active-class'
+    routes: routes,
+    linkActiveClass: 'active'
 });
 
 //Vue.config.silent = false;
