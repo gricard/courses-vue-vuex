db28ee66287c05ce7fce831565302fe6592ca707
Gabriel Ricard <sumorai@gmail.com>
2016-10-20 16:12:20 -0400

    fixed

--- src/main.js
+++ src/main.js
@@ -16,7 +16,6 @@ import store from './vuex/store';
 // link store & router
 // this requires vuex-router-sync v3.0.x or greater
 sync(store, router);
-// TODO not working yet
 
 //Vue.config.silent = false;
 //Vue.config.devtools = true;
