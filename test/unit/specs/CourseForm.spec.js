71d39202b244cadf139bb9fe090ca11d25a40a66
Gabriel Ricard <sumorai@gmail.com>
2016-11-05 21:35:17 -0400

    completed working tests from react/redux for this component

--- test/unit/specs/CourseForm.spec.js
+++ test/unit/specs/CourseForm.spec.js
@@ -8,6 +8,7 @@ import SelectInput from 'src/components/common/SelectInput'
 Vue.component('TextInput', TextInput);
 Vue.component('SelectInput', SelectInput);
 
+// component mocking function
 function setup(saving, deleting) {
   const propsData = {
     course: {},
@@ -20,107 +21,42 @@ function setup(saving, deleting) {
     onChange: () => {}
   };
 
-  const Ctor = Vue.extend(CourseForm)
+  const Ctor = Vue.extend(CourseForm);
   const vm = new Ctor({
-//    el: document.createElement('div'),
     propsData
-  })
-  vm.$mount()
-
-
-//  const vm = new Vue({
-//    template: '<div><CourseForm :course="course" :errors="errors" :allAuthors="allAuthors" :onSave="onSave" :onChange="onChange" :onDelete="onDelete" :saving="saving" :deleting="deleting" /></div>',
-//    components: {
-//      'CourseForm': CourseForm
-//    },
-//    propsData: propsData
-//  })
-//  vm.$mount()
-
-
-//  const vm = new Vue({
-//    el: document.createElement('div'),
-//    propsData: propsData,
-//    render: (h) => h(CourseForm)
-//  });
-
+  });
+  vm.$mount();
 
   return vm;
 }
 
+// actual tests
 describe('CourseForm.vue', () => {
-//  it('renders form and h1', () => {
-//    const wrapper = setup(false);
-//    expect(wrapper.find('form').length).toBe(1);
-//    expect(wrapper.find('h1').text()).toEqual('Manage Course');
-//  });
-
   it('renders form and h1', () => {
     const vm = setup(false, false);
-//    console.log('el', vm.$el);
-//    console.log('form', vm.$el.querySelectorAll('form'));
-//    console.log('h1', vm.$el.querySelector('h1'));
-    expect(vm.$el.querySelectorAll('form').length).to.equal(1)
-    expect(vm.$el.querySelector('h1').textContent).to.equal('Manage Course')
-  })
-
-//  it('should render correct contents', () => {
-//    const vm = new Vue({
-//      el: document.createElement('div'),
-//      render: (h) => h(Hello)
-//    })
-//    expect(vm.$el.querySelector('.hello h1').textContent)
-//        .to.equal('Welcome to Your Vue.js App')
-//  })
-})
+    expect(vm.$el.querySelectorAll('form').length).to.equal(1);
+    expect(vm.$el.querySelector('h1').textContent).to.equal('Manage Course');
+  });
 
 
+  it('save button is labeled "Save" when not saving', () => {
+    const vm = setup(false, false);
+    expect(vm.$el.querySelector('input.saver').value).to.equal('Save');
+  });
 
-//
-//import expect from 'expect';
-//import React from 'react';
-//import {mount, shallow} from 'enzyme';
-//import TestUtils from 'react-addons-test-utils';
-//import CourseForm from './CourseForm';
-//
-//function setup(saving, deleting) {
-//  const props = {
-//    course: {},
-//    saving: saving,
-//    deleting: deleting,
-//    errors: {},
-//    onSave: () => {},
-//    onDelete: () => {},
-//    onChange: () => {}
-//  };
-//
-//  return shallow(<CourseForm {...props} />);
-//}
-//
-//describe('CourseForm via Enzyme', () => {
-//  it('renders form and h1', () => {
-//    const wrapper = setup(false);
-//    expect(wrapper.find('form').length).toBe(1);
-//    expect(wrapper.find('h1').text()).toEqual('Manage Course');
-//  });
-//
-//  it('save button is labeled "Save" when not saving', () => {
-//    const wrapper = setup(false, false);
-//    expect(wrapper.find('input.saver').props().value).toBe('Save');
-//  });
-//
-//  it('save button is labeled "Saving..." when saving', () => {
-//    const wrapper = setup(true, false);
-//    expect(wrapper.find('input.saver').props().value).toBe('Saving...');
-//  });
-//
-//  it('delete button is labeled "Delete" when not deleting', () => {
-//    const wrapper = setup(false, false);
-//    expect(wrapper.find('input.deleter').props().value).toBe('Delete');
-//  });
-//
-//  it('delete button is labeled "Deleting..." when deleting', () => {
-//    const wrapper = setup(false, true);
-//    expect(wrapper.find('input.deleter').props().value).toBe('Deleting...');
-//  });
-//});
+  it('save button is labeled "Saving..." when saving', () => {
+    const vm = setup(true, false);
+    expect(vm.$el.querySelector('input.saver').value).to.equal('Saving...');
+  });
+
+  it('delete button is labeled "Delete" when not deleting', () => {
+    const vm = setup(false, false);
+    expect(vm.$el.querySelector('input.deleter').value).to.equal('Delete');
+  });
+
+  it('delete button is labeled "Deleting..." when deleting', () => {
+    const vm = setup(false, true);
+    expect(vm.$el.querySelector('input.deleter').value).to.equal('Deleting...');
+  });
+});
+
