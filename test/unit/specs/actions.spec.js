225a89ec396a8135eb0ef32cfde171b872dbd4dd
Gabriel Ricard <sumorai@gmail.com>
2016-11-15 16:00:52 -0500

    updated for changes to testAction

--- test/unit/specs/actions.spec.js
+++ test/unit/specs/actions.spec.js
@@ -4,10 +4,10 @@
 import { actions } from 'src/store/actions';
 import store from 'src/store/store';
 const state = store.state;
-import {testAction} from './helper';
-
-console.log('actions', actions);
-console.log('UPDATE_ERRORS', actions.UPDATE_ERRORS);
+import {testAction} from './testAction';
+//
+//console.log('actions', actions);
+//console.log('UPDATE_ERRORS', actions.UPDATE_ERRORS);
 
 describe('UPDATE_ERRORS action.', () => {
     beforeEach(() => {
@@ -19,7 +19,7 @@ describe('UPDATE_ERRORS action.', () => {
 
     it('Should dispatch the right mutations.', (done) => {
         testAction(actions.UPDATE_ERRORS, {errors: {title: 'the title is wrong'}}, state, [{
-                name: 'SET_ERRORS',
+                type: 'SET_ERRORS',
                 payload: {title: 'the title is wrong'}
             }], [
                 // no actions expected
