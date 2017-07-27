9e0bcccddebd433d787c4f4e430c318a304a7f3e
Gabriel Ricard <sumorai@gmail.com>
2017-07-26 21:25:02 -0400

    Add mutations unit tests

--- test/unit/specs/mutations.spec.js
+++ test/unit/specs/mutations.spec.js
@@ -4,7 +4,21 @@ import { mutations } from '@/store/mutations';
 
 
 // destructure assign mutations
-const { SET_COURSES, DECREMENT_AJAX_CALLS } = mutations;
+const {
+    SET_COURSES,
+    SET_COURSE,
+    SET_AUTHOR,
+    SET_AUTHORS,
+    INCREMENT_AJAX_CALLS,
+    DECREMENT_AJAX_CALLS,
+    SET_LOADING_FRAME,
+    SET_SAVING,
+    SET_DELETING,
+    SET_ERRORS,
+    SET_DIRTY,
+    LOAD_COURSE,
+    LOAD_AUTHOR
+} = mutations;
 
 describe('Mutations', () => {
     describe('SET_COURSES', () => {
@@ -21,6 +35,61 @@ describe('Mutations', () => {
         });
     });
 
+    describe('SET_COURSE', () => {
+        it('Should modify state.course', () => {
+            // mock state
+            const state = { course: null };
+
+            // apply mutation
+            const course = { title: 'my course'};
+            SET_COURSE(state, { course });
+
+            // assert result
+            expect(state.course).to.equal(course);
+        });
+    });
+
+    describe('SET_AUTHORS', () => {
+        it('Should modify state.authors', () => {
+            // mock state
+            const state = { authors: null };
+
+            // apply mutation
+            const authors = { 1: { name: 'bob'} };
+            SET_AUTHORS(state, { authors });
+
+            // assert result
+            expect(state.authors).to.equal(authors);
+        });
+    });
+
+    describe('SET_AUTHOR', () => {
+        it('Should modify state.author', () => {
+            // mock state
+            const state = { author: null };
+
+            // apply mutation
+            const author = { name: 'bob'};
+            SET_AUTHOR(state, { author });
+
+            // assert result
+            expect(state.author).to.equal(author);
+        });
+    });
+
+    describe('INCCREMENT_AJAX_CALLS', () => {
+        it('Should modify state.ajaxCallsInProgress and state.ajaxError', () => {
+            // mock state
+            const state = { ajaxCallsInProgress: 0, ajaxError: null };
+
+            // apply mutation
+            INCREMENT_AJAX_CALLS(state);
+
+            // assert result
+            expect(state.ajaxCallsInProgress).to.equal(1);
+        });
+    });
+
     describe('DECREMENT_AJAX_CALLS', () => {
         it('Should modify state.ajaxCallsInProgress and state.ajaxError', () => {
             // mock state
@@ -32,6 +101,97 @@ describe('Mutations', () => {
             // assert result
             expect(state.ajaxCallsInProgress).to.equal(0);
             expect(state.ajaxError).to.equal('it did not work');
+        });
+    });
+
+    describe('SET_LOADING_FRAME', () => {
+        it('Should modify state.loadingFrame', () => {
+            // mock state
+            const state = { ajaxCallsInProgress: 1, ajaxError: null, loadingFrame: null };
+
+            // apply mutation
+            SET_LOADING_FRAME(state, { frame: 'testVal' });
+
+            // assert result
+            expect(state.loadingFrame).to.equal('testVal');
+        });
+    });
+
+    describe('SET_SAVING', () => {
+        it('Should modify state.saving', () => {
+            // mock state
+            const state = { saving: null };
+
+            // apply mutation
+            SET_SAVING(state, true);
+
+            // assert result
+            expect(state.saving).to.equal(true);
+        });
+    });
+
+    describe('SET_DELETING', () => {
+        it('Should modify state.deleting', () => {
+            // mock state
+            const state = { deleting: null };
+
+            // apply mutation
+            SET_DELETING(state, true);
+
+            // assert result
+            expect(state.deleting).to.equal(true);
+        });
+    });
+
+    describe('SET_ERRORS', () => {
+        it('Should modify state.errors', () => {
+            // mock state
+            const state = { errors: null };
+
+            // apply mutation
+            SET_ERRORS(state, true);
+
+            // assert result
+            expect(state.errors).to.equal(true);
+        });
+    });
+
+    describe('SET_DIRTY', () => {
+        it('Should modify state.dirty', () => {
+            // mock state
+            const state = { dirty: null };
+
+            // apply mutation
+            SET_DIRTY(state, true);
+
+            // assert result
+            expect(state.dirty).to.equal(true);
+        });
+    });
+
+    describe('LOAD_COURSE', () => {
+        it('Should modify state.course', () => {
+            // mock state
+            const state = { course: null };
+
+            // apply mutation
+            LOAD_COURSE(state, true);
+
+            // assert result
+            expect(state.course).to.equal(true);
+        });
+    });
+
+    describe('LOAD_AUTHOR', () => {
+        it('Should modify state.author', () => {
+            // mock state
+            const state = { author: null };
+
+            // apply mutation
+            LOAD_AUTHOR(state, true);
+
+            // assert result
+            expect(state.author).to.equal(true);
         });
     });
 });
