ecdec72d41828782247d959c5434c6477a1c272a
Gabriel Ricard <sumorai@gmail.com>
2017-07-26 22:08:11 -0400

    Better use of avoriaz syntax

--- test/unit/specs/AuthorForm.spec.js
+++ test/unit/specs/AuthorForm.spec.js
@@ -33,27 +33,27 @@ describe('AuthorForm.vue', () => {
   it('renders form and h1', () => {
     const vm = setup(false, false);
     expect(vm.find('form').length).to.equal(1);
-    expect(vm.find('h1')[0].text()).to.equal('Manage Author');
+    expect(vm.first('h1').text()).to.equal('Manage Author');
   });
 
   it('save button is labeled "Save" when not saving', () => {
     const vm = setup(false, false);
-    expect(vm.find('input.saver')[0].hasAttribute('value', 'Save')).to.equal(true);
+    expect(vm.first('input.saver').hasAttribute('value', 'Save')).to.equal(true);
   });
 
   it('save button is labeled "Saving..." when saving', () => {
     const vm = setup(true, false);
-    expect(vm.find('input.saver')[0].hasAttribute('value', 'Saving...')).to.equal(true);
+    expect(vm.first('input.saver').hasAttribute('value', 'Saving...')).to.equal(true);
   });
 
   it('delete button is labeled "Delete" when not deleting', () => {
     const vm = setup(false, false);
-    expect(vm.find('input.deleter')[0].hasAttribute('value', 'Delete')).to.equal(true);
+    expect(vm.first('input.deleter').hasAttribute('value', 'Delete')).to.equal(true);
   });
 
   it('delete button is labeled "Deleting..." when deleting', () => {
     const vm = setup(false, true);
-    expect(vm.find('input.deleter')[0].hasAttribute('value', 'Deleting...')).to.equal(true);
+    expect(vm.first('input.deleter').hasAttribute('value', 'Deleting...')).to.equal(true);
   });
 });
 
