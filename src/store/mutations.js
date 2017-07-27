b9b16faa3860bfb85e2299418d48acb34db82de3
Gabriel Ricard <sumorai@gmail.com>
2017-07-26 21:25:26 -0400

    Made mutations easier to determine code coverage for

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,58 +1,70 @@
 
+const SET_COURSES = (state, { courses }) => {
+    state.courses = courses;
+};
+
+const SET_COURSE = (state, { course }) => {
+    state.course = course;
+};
+
+const SET_AUTHORS = (state, { authors }) => {
+    state.authors = authors;
+};
+
+const SET_AUTHOR = (state, { author }) => {
+    state.author = author;
+};
+
+const INCREMENT_AJAX_CALLS = (state) => {
+    state.ajaxCallsInProgress++;
+};
+
+const DECREMENT_AJAX_CALLS = (state, params) => {
+    state.ajaxCallsInProgress--;
+
+    if (params && params.error) state.ajaxError = params.error;
+};
+
+const SET_LOADING_FRAME = (state, { frame }) => {
+    state.loadingFrame = frame;
+};
+
+const SET_SAVING = (state, saving) => {
+    state.saving = saving;
+};
+
+const SET_DELETING = (state, deleting) => {
+    state.deleting = deleting;
+};
+
+const SET_ERRORS = (state, errors) => {
+    state.errors = errors;
+};
+
+const SET_DIRTY = (state, dirty) => {
+    state.dirty = dirty;
+};
+
+const LOAD_COURSE = (state, course) => {
+    state.course = course;
+};
+
+const LOAD_AUTHOR = (state, author) => {
+    state.author = author;
+};
+
 export const mutations = {
-
-    SET_COURSES (state, { courses }) {
-        state.courses = courses;
-    },
-
-    SET_COURSE (state, { course }) {
-        state.course = course;
-    },
-
-    SET_AUTHORS (state, { authors }) {
-        state.authors = authors;
-    },
-
-    SET_AUTHOR (state, { author }) {
-        state.author = author;
-    },
-
-    INCREMENT_AJAX_CALLS (state) {
-        state.ajaxCallsInProgress++;
-    },
-
-    DECREMENT_AJAX_CALLS (state, params) {
-        state.ajaxCallsInProgress--;
-
-        if (params && params.error) state.ajaxError = params.error;
-    },
-
-    SET_LOADING_FRAME (state, { frame }) {
-        state.loadingFrame = frame;
-    },
-
-    SET_SAVING (state, saving) {
-        state.saving = saving;
-    },
-
-    SET_DELETING (state, deleting) {
-        state.deleting = deleting;
-    },
-
-    SET_ERRORS (state, errors) {
-        state.errors = errors;
-    },
-
-    SET_DIRTY (state, dirty) {
-        state.dirty = dirty;
-    },
-
-    LOAD_COURSE (state, course) {
-        state.course = course;
-    },
-
-    LOAD_AUTHOR(state, author) {
-        state.author = author;
-    }
-
+    SET_COURSES,
+    SET_COURSE,
+    SET_AUTHOR,
+    SET_AUTHORS,
+    INCREMENT_AJAX_CALLS,
+    DECREMENT_AJAX_CALLS,
+    SET_LOADING_FRAME,
+    SET_SAVING,
+    SET_DELETING,
+    SET_ERRORS,
+    SET_DIRTY,
+    LOAD_COURSE,
+    LOAD_AUTHOR
 };
